104329b6e7b33454e849cc09fabe4225995e85d3
Keith <kityen5r@gmail.com>
2018-07-11 12:40:27 +0800

    Added code for s3 implementation

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,7 +7,7 @@ CarrierWave.configure do |config|
       :provider => ‘AWS’,
       :aws_access_key_id => ENV[‘AWS_ACCESS_KEY_ID’],
       :aws_secret_access_key => ENV[‘AWS_SECRET_ACCESS_KEY’],
-      :region => ‘ap-northeast-1’ # Tokyo
+      :region => ‘asia-singapore’ # Tokyo
     }
 
     config.fog_directory = ENV[‘S3_BUCKET_NAME’]
